8b16b9e0d58b8b01c1750cc7786f19189aaaee05
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-09-19 14:13:30 +0400

    Update sentiment_analysis.py (Add comment for line 5 )

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,6 +3,6 @@ import requests # Import the requests library to handle HTTP requests
 def sentiment_analyzer(text_to_analyse): # Define the function 'sentiment_analyzer' to take the string input (text_to_analyse)
     url ='https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  # URL for the sentiment analysis service
     myobj = {"raw_document": {"text": text_to_analyse}}  # Create the dictionary 'myobj' with the text to be analyzed under the variable 'text'
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"} # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers
     return response.text # Return the response text from the API
