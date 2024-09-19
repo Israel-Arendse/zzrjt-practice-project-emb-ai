1f214d8add1f00ee8fc958422ba248ca79abc7a7
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-09-19 11:53:53 -0400

    Rewrote 'sentiment_analyzer' to return dictionary

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,8 +1,20 @@
 import requests # Import the requests library to handle HTTP requests
+import json     # Import the json library
 
 def sentiment_analyzer(text_to_analyse): # Define the function 'sentiment_analyzer' to take the string input (text_to_analyse)
     url ='https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  # URL for the sentiment analysis service
     myobj = {"raw_document": {"text": text_to_analyse}}  # Create the dictionary 'myobj' with the text to be analyzed under the variable 'text'
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"} # Set the headers required for the API request
-    response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers
-    return response.text # Return the response text from the API
+
+    # Send a POST request to the API with the text and headers
+    response = requests.post(url, json = myobj, headers=header)
+
+    # Parse the JSON response
+    formatted_response = json.loads(response.text)
+
+    # Extract the  label and score
+    label = formatted_response['documentSentiment']['label']
+    score = formatted_response['documentSentiment']['score']
+
+    # Return turn a dictionary with label and score
+    return {'label': label, 'score': score}
