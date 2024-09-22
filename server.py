cf1938aacdc56024e1067b365eed6c8540878cc0
Israel-Arendse <israelfortnite@gmail.com>
2024-09-22 05:07:40 -0400

    Fixed return string in line 41

--- server.py
+++ server.py
@@ -2,11 +2,11 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package
-from flask import Flask, render_template, request
-
 # Import the sentiment_analyzer function from the package created
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
+# Import Flask, render_template, request from the flask pramework package
+from .flask import Flask, render_template, request
 
 # Initiate the flask app
 app = Flask("Sentiment Analyzer")
@@ -16,11 +16,11 @@ app = Flask("Sentiment Analyzer")
 # 2: Calls the 'sentiment_analyzer' application with 'text_to_analyze' as an argument
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
+    ''' This code receives the text from the HTML interface and
         runs sentiment analysis over it using sentiment_analysis()
         function. The code uses an 'if' and 'elif' function to check
-        if the input is valid. The output returned shows the label and 
-        its confidence score for the provided text. If the input is invalid 
+        if the input is valid. The output returned shows the label and
+        its confidence score for the provided text. If the input is invalid
         then the output returns the message 'Invalid input! Try again.'
     '''
     # Retrieve the text to analyze from the requests arguments
@@ -36,10 +36,9 @@ def sent_analyzer():
      # Check if the label is None, indicating an error or invalid input
     if label is None:
         return "Invalid input! Try again."
-    
     else:
-         # Return a formmatted string with the sentiment label and score
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+        # Return a formatted string with the sentiment label and score
+        return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 
 @app.route("/")
 def render_index_page():  # Runs the 'render_template' function in the 'index.html' HTML template
@@ -52,4 +51,4 @@ def render_index_page():  # Runs the 'render_template' function in the 'index.ht
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''
-    app.run(host="0.0.0.0", port=5000) # Hosts the app on the port:5000
+    app.run(host="0.0.0.0", port=5000) # Hosts the app on the port:5000.
