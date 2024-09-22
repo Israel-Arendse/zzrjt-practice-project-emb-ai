5b728370e088f8ea67ac2de08e74483d473cc720
Israel-Arendse <israelfortnite@gmail.com>
2024-09-22 03:24:12 -0400

    Remove side comment in line 13 of 'sentiment_analysis.py'

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -10,8 +10,8 @@ def sentiment_analyzer(text_to_analyse):
     myobj = {"raw_document": {"text": text_to_analyse}} 
 
     # Set the headers required for the API request
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"} # Set the headers required for the API request
-
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"} 
+    
     # Send a POST request to the API with the text and headers
     response = requests.post(url, json = myobj, headers=header)
 
