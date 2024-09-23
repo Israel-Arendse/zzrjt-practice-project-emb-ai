4426c2beef6984e1accc7469b7143a657ccf04d0
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-09-23 12:00:18 +0400

    Update sentiment_analysis.py (Added function docstring)
    I have removed the white space in line 19 and added a function docstring to 'def sentiment_analyzer'.

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -8,6 +8,18 @@ import requests  # Import the requests library to handle HTTP requests
 
 # Define the function 'sentiment_analyzer' to take the string input (text_to_analyse)
 def sentiment_analyzer(text_to_analyse):
+    '''
+    Analyzes the text input and return the dictionary
+    with the status code 200. If an error is encountered
+    then the response status code return is 500.
+
+    Parameters:
+     'label' (key-value): The type of sentiment detected.
+     'score' (key-value): The score rating for the sentiment.
+
+    Returns:
+     dictionary: The type of sentiment in the text and its score.
+    '''
     # Define URL for the sentiment analysis service
     url ='https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
 
@@ -16,7 +28,7 @@ def sentiment_analyzer(text_to_analyse):
 
     # Set the headers required for the API request
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    
+
     # Send a POST request to the API with the text and headers
     response = requests.post(url, json = myobj, headers=header)
 
