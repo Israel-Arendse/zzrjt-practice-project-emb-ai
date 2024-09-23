e743dbac403c25ba130832c9bef4407b1af6840e
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-09-23 10:22:22 +0400

    Update sentiment_analysis.py (Modified sentiment_analyis.py file)
    In this update, I had to make some changes to 'sentiment_analysis.py'.
    
    1. I removed trailing whitespace from lines 10, 13, and 14.
    
    2. I placed the import "JSON" file before the third-party import "requests" as stated in the import order rules.

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,5 +1,5 @@
-import requests # Import the requests library to handle HTTP requests
-import json     # Import the json library
+import json      # Import the JSON library
+import requests  # Import the requests library to handle HTTP requests
     
 # Define the function 'sentiment_analyzer' to take the string input (text_to_analyse)
 def sentiment_analyzer(text_to_analyse): 
@@ -7,10 +7,10 @@ def sentiment_analyzer(text_to_analyse):
     url ='https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  
 
     # Create the payload with the text to be analyzed
-    myobj = {"raw_document": {"text": text_to_analyse}} 
+    myobj = {"raw_document": {"text": text_to_analyse}}
 
     # Set the headers required for the API request
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"} 
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     
     # Send a POST request to the API with the text and headers
     response = requests.post(url, json = myobj, headers=header)
